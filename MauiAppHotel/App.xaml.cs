da06a1f2660310313ed5af3bd6fe338de644ddc0
Diego Oliveira <diegofso93@gmail.com>
2026-05-20 00:10:04 -0300

    Aula 13 concluída

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -15,7 +15,7 @@
             if (window != null)
             {
                 window.Width = 400;
-                window.Height = 600;
+                window.Height = 700;
             }
 
             return window!;

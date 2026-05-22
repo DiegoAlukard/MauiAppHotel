70c6c8689d34a735b547054b9b35543a3db3633f
Diego Oliveira <diegofso93@gmail.com>
2026-05-21 22:40:55 -0300

    Pagina 2

--- MauiAppHotel/Views/HospedagemContratada.xaml.cs
+++ MauiAppHotel/Views/HospedagemContratada.xaml.cs
@@ -2,8 +2,21 @@ namespace MauiAppHotel.Views;
 
 public partial class HospedagemContratada : ContentPage
 {
-	public HospedagemContratada()
-	{
-		InitializeComponent();
-	}
+    public HospedagemContratada()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+        
+            Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("ops", ex.Message, "OK");
+        }
+    }
 }
